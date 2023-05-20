3b3e1bb08e67a63bafd4e3a298b4add1096b4178
bk408 <bhavyakhatri1912@gmail.com>
2023-05-20 22:33:02 +0530

    recursion

---
+++ script.js
@@ -0,0 +1,10 @@
+function factorial(item){
+    console.log("item", item)
+    if(item==0){
+        return 1
+    }
+    return item*factorial(item-1)
+}
+
+let data = 5;
+console.log(factorial(data))
